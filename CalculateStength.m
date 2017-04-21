947d9652499ef19ec424789a08cb96b8e537e7b1
Andrew Grace <a2grace@uwaterloo.ca>
2017-04-21 16:13:57 -0400

    Modified CalculateStrength a bit

--- CalculateStength.m
+++ CalculateStength.m
@@ -1,7 +1,7 @@
 
 clear
-data_path = strcat('/Volumes/','Ext. Drive','/seiche2D/2048x1024/Amp05/h005/');
-save_path = strcat('/Volumes/','Ext. Drive','/seiche2D/Notes/');
+data_path = strcat('/Volumes/','Ext. Drive','/seiche2D/L5');
+save_path = strcat('/Volumes/','Ext. Drive','/Data/');
 cd(data_path)
 gdpar = spins_gridparams('vector',false); split_gdpar; par2var(params);
 
@@ -11,7 +11,7 @@ numbins = delta_rho/dataspacing;
 varmag = zeros(numouts+1,numbins);
 %%
 
-for ii = 0:10:numouts
+for ii = 0:1:numouts
     disp(['Output number: ' num2str(ii)])
     tic
     vari = spins_reader_new('rho',ii);
@@ -30,6 +30,6 @@ end
 
 varmag = varmag(any(varmag,2),:);
 s = linspace(lims(1),lims(2),numbins);
-t = linspace(0,final_time,length(0:10:numouts));
+t = linspace(0,final_time,length(0:1:numouts));
 
 [ss,tt] = meshgrid(s,t);
