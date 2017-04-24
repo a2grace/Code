55e24b830d9b1ec015cd026159159779d8a7e676
Andrew Grace <a2grace@uwaterloo.ca>
2017-04-24 10:04:26 -0400

    Add magnitude spacetime plots

--- hov_2D.m
+++ hov_2D.m
@@ -1,30 +1,22 @@
-%%%%%%%%%%% MOST RECENT UPLOAD TO SHARCNET: APRIL 6th, 2017
+%%%%%%%%%% MOST RECENT UPLOAD TO SHARCNET: APRIL 24th, 2017
 %%%%%%%%%%% %%%%%%%%%%%%%%%
 clear
 close all
 
-save_path = '/work/a2grace/hov';
+save_path = '/work/a2grace/Data';
 
-
-% field1 = 'L5'; value1 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp0825/h005';
-% field2 = 'M5'; value2 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp065/h005';
-% field3 = 'S5'; value3 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp05/h005';
-% 
-% field4 = 'L25'; value4 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp0825/h025';
-% field5 = 'M25'; value5 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp065/h025';
-% field6 = 'S25'; value6 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp05/h025';
-
-field1 = 'control_S5'; value1 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp05/h005';
-field2 = 'control_M5'; value2 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/Amp065/h005'
-%field2 = 'n2'; value2 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/perturb/himode/n2';
-%field3 = 'n4'; value3 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/perturb/himode/n4';
-%field4 = 'n5'; value4 = '/scratch/kglamb/a2grace/seichefiles/seiche2D/2048x1024/FilterD/perturb/himode/n5';
+field1 = 'S5_spacetime'; value1 = '/scratch/kglamb/a2grace/S5';
+field2 = 'M5_spacetime'; value2 = '/scratch/kglamb/a2grace/M5';
+field3 = 'L5_spacetime'; value3 = '/scratch/kglamb/a2grace/L5';
+field4 = 'Sn2_spacetime'; value4 = '/scratch/kglamb/a2grace/Sn2';
+field5 = 'Sn4_spacetime'; value5 = '/scratch/kglamb/a2grace/Sn4';
+field6 = 'Sn5_spacetime'; value6 = '/scratch/kglamb/a2grace/Sn5';
 
 
 
-myfields = {field1 field2};
+myfields = {field1 field2 field3 field4 field5 field6};
 
-mypaths = {value1 value2};
+mypaths = {value1 value2 value3 value4 value5 value6};
 
 
 
@@ -37,12 +29,20 @@ for jj = 1:length(myfields)
     gdpar = spins_gridparams('vector',false); split_gdpar;
     par2var(params);
     numouts = final_time/plot_interval; 
+    
+    %Event Fields
     rhosum = zeros(Nx,numouts+1);
     KEsum = rhosum; 
     mixsum = rhosum;
     uz = rhosum;
-    varsum = rhosum;    
-
+    varsum = rhosum;   
+    
+    %Magnitude Fields
+    mixing = rhosum;
+    vertical_shear = rhosum;
+    variability = rhosum;
+    
+    
     rholow = -0.5*delta_rho*tanh(1)*(1+myeps);
     rhohigh = -rholow;
     period = 20;
@@ -71,24 +71,34 @@ for jj = 1:length(myfields)
         rhoz = even_y_deriv(rho,[],Lz,[]);
         rhoxx = odd_x_deriv(rhox,Lx,[],[]);
         rhozz = odd_y_deriv(rhoz,[],Lz,[]);
-	dummy = -kappa_rho*(rhoxx + rhozz).^2;
-	mixind = (dummy<-kappa_rho*(delta_rho)^2/(2*h_halfwidth)^4); 
+        dummy = -kappa_rho*(rhoxx + rhozz).^2;
+        mixind = (dummy<-kappa_rho*(delta_rho)^2/(2*h_halfwidth)^4); 
         mixsum(:,ii+1) = sum(mixind,2);
         
-        %%%Variability%%%i
+        %%%Variability--Events%%%
         dummy = rhox.^2 + rhoz.^2;
         varsum(:,ii+1) = sum(dummy>0.9*(delta_rho/(2*h_halfwidth))^2,2);%find the order of magnitude of the variability
 
-	%%%Vertical Shear%%%
-	coeff = 4*eta_0/(period*Lz*2*h_halfwidth);
-	dummy = spins_reader('u',ii);
-	dummy = even_y_deriv(dummy,[],Lz,[]);
-	uz(:,ii+1) = sum(abs(dummy)>coeff,2); %4*eta_0/(HT2*h_halfwidth)
+        %%%Vertical Shear--Events%%%
+        coeff = 4*eta_0/(period*Lz*2*h_halfwidth);
+        dummy = spins_reader('u',ii);
+        dummy = even_y_deriv(dummy,[],Lz,[]);
+        uz(:,ii+1) = sum(abs(dummy)>coeff,2); %4*eta_0/(HT2*h_halfwidth)
+        
+        %%%Variability--Magnitude%%%
+        dummy = rhox.^2 + rhoz.^2;
+        variability(:,ii+1) = sum(dummy,2);
+
+        %%%Vertical Shear--Magnitude%%%
+        dummy = spins_reader('u',ii);
+        dummy = even_y_deriv(dummy,[],Lz,[]);
+        vertical_shear(:,ii+1) = sum(abs(dummy),2); %4*eta_0/(HT2*h_halfwidth)
     end
-    rhosum = rhosum/Nz;
     KEsum = 0.5*rho_0*KEsum;
+    
     hov = struct('rhosum',rhosum,'KEsum',KEsum,'mixsum',mixsum,...
-        'varsum',varsum,'uz',uz);
+        'varsum',varsum,'uz',uz,'variability',variability,...
+        'vertical_shear',vertical_shear);
 
     newdata = fieldnames(params);
         for index = 1:length(newdata)
